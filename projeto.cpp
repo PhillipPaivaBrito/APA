7f0d993f9e6708e8fccec1bf25ded707a1ac69ce
PhillipPaivaBrito <phillippaivabrito@gmail.com>
2025-04-15 14:23:34 -0300

    Initial commit

---
+++ projeto.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <limits>
+
+class solver {
+    public:
+
+    int custtotal = 0;
+    std::vector<std::vector<int>> rotas;
+
+    int numero_de_voos,
+        numero_de_pistas;
+
+    std::vector <int> r;//termpo minimo para iniciar operação
+    std::vector <int> c;//custo para completar operação
+    std::vector <int> p;//custo de espera
+    std::vector<std::vector<int>> t;//tempo de espera entre j e i
+
+    std::vector<int> list; // termpo minimo para iniciar operação
+
+    solver(int numero_de_voos, int numero_de_pistas,std::vector<int> &r,std::vector<int> &c, std::vector<int> &p, std::vector<std::vector<int>> &t){
+        this-> numero_de_voos = numero_de_voos;
+        this->numero_de_pistas = numero_de_pistas;
+        this->r = r;
+        this->c = c;
+        this->p = p;
+        this->t = t;
+        this->list.swap(r);
+        makePlaciment();
+    }
+    void makePlaciment(){
+        //fazer heuristica para alocar os voos nas pistas
+        //alocar os voos nas pistas de acordo com o tempo minimo para iniciar operação
+        //alocar os voos nas pistas de acordo com o custo para completar operação
+        //alocar os voos nas pistas de acordo com o custo de espera
+
+        rotas.resize(numero_de_pistas);
+        int i = 0;
+        do{
+            for(int j = 0; j < numero_de_pistas; j++,i++){
+                if(i == numero_de_voos){
+                    std::cout << std::endl << "Todos os voos alocados" << std::endl;
+                    break;
+                }else{
+                    if(int(rotas[j].size()) < 1){
+                        this->rotas[j].push_back(findshorterindex());
+                    }else{
+                        //aqui vai outro criterio de alocação
+                        //alocar o voo na pista que tem o menor tempo de espera
+
+                        this->rotas[j].push_back(findshorterindex());
+                    }
+                }
+                
+            }
+        }while(i < numero_de_voos);
+        std::cout << std::endl << "Rotas: " << std::endl;
+        for(int i = 0; i < numero_de_pistas; i++){
+            std::cout << "Pista " << i+1 << ": ";
+            for(int j = 0; j < int(rotas[i].size()); j++){
+                std::cout << rotas[i][j] << ' ';
+            }
+            std::cout << std::endl;
+        }
+        
+    }
+    int findshorterindex(){
+        int menor = 0;
+        for (int i = 1; i < numero_de_voos; i++){
+            //std::cout << "list[i]: " << this->list[i] << std::endl;
+            if(this->list[i] < this->list[menor]){
+                menor = i;
+                
+            }
+        }
+        //std::cout <<std::endl << "menor: " << menor;
+        this->list[menor] = std::numeric_limits<int>::max(); // marca o menor como maximo para não ser escolhido novamente
+        return menor;
+    }
+    int calvalue(){
+        
+        return 0;
+    }
+
+    int getr(int row){
+        return this->r[row];
+    }
+    int getc(int row){
+        return this->c[row];
+    }
+    int gett(int row, int col){
+        return this->t[row][col];
+    }
+};
+
+int main(int argc, char *argv[]){
+
+    std::string arquivo = "../instancias_teste/";
+    int content;
+
+    int numero_de_voos,
+        numero_de_pistas;
+
+    std::vector <int> r;//tempo minimo para iniciar operação
+    std::vector <int> c;//custo para completar operação
+    std::vector <int> p;//custo de espera
+    std::vector<std::vector<int>> t;//tempo de espera entre j e i
+    //abrir arquivo
+    std::ifstream myfile(arquivo + "n3m10A.txt"); //std::ifstream myfile(argv[1]);
+    if(argc > 1){
+        myfile.close(); // fecha o arquivo
+        myfile.clear(); // limpa o estado do arquivo
+        myfile.open(arquivo + argv[1]);
+    }
+      
+    if (!(myfile.is_open()))  // Se houve erro na abertura
+    {
+        std::cout << "Problemas na abertura do arquivo\n" << myfile.is_open();
+        return 0;
+    }
+    std::cout << "Arquivo aberto com sucesso\n";
+    myfile >> numero_de_voos;
+    myfile >> numero_de_pistas;
+
+    std::cout << numero_de_voos << std::endl << numero_de_pistas << std::endl<<std::endl;
+
+    for(int i = 0; i < numero_de_voos; i++){// leitura tempo minimo para iniciar operação
+        myfile >> content;
+        r.push_back(content);
+        std::cout << r.back() <<' ';
+    }
+    std::cout<<std::endl;
+    for(int i = 0; i < numero_de_voos; i++){// leitura dos custo para completar operação
+        myfile >> content;
+        c.push_back(content);
+        std::cout << c.back() <<' ';
+    }
+    std::cout<<std::endl;
+    for(int i = 0; i < numero_de_voos; i++){// leitura dos custo de espera
+        myfile >> content;
+        p.push_back(content);
+        std::cout << p.back() <<' ';
+    }
+    std::cout<<std::endl<<std::endl;;
+
+    t.resize(numero_de_voos, std::vector<int>(numero_de_voos)); // Redimensiona t para ser uma matriz numero_de_voos x numero_de_voos
+    
+    for(int i = 0; i < numero_de_voos; i++){// leitura da matriz de tempo de espera entre j e i
+        for(int j = 0; j < numero_de_voos; j++){
+            myfile >> content;
+            t[i][j] = content;
+            std::cout << t[i][j]<<' ';
+        }
+        std::cout<<std::endl;
+    }
+    myfile.close(); // fecha o arquivo
+    solver s(numero_de_voos, numero_de_pistas, r, c, p, t);
+    std::cout << std::endl << "Custos totais: " << s.custtotal << std::endl;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+    for(int i=0; i < rotas[j].size; i++){
+                std::cout<<this->rotas[0][i]<<" ";
+            }//*/
